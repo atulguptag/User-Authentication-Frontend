8ae545ba0ad54eefc8017dc5f8668153d5381514
Atul Gupta <atulguptag111@gmail.com>
2024-04-20 12:13:29 +0530

    Create Login.jsx

---
+++ src/components/login-page/Login.jsx
@@ -0,0 +1,69 @@
+import "./login-page.css";
+import { useState, useContext } from "react";
+import AuthContext from "../../context/AuthContext";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Assuming Font Awesome
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons"; // Font Awesome icons
+
+
+const Login = () => {
+  const { user, loginUser } = useContext(AuthContext);
+
+  const [showPassword, setShowPassword] = useState(false);
+  const handleTogglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+    console.log("showPassword:", showPassword); // Log state for debugging
+  };
+
+  return user ? ( // Check if user is truthy (logged in)
+    <h1>You are already logged in.</h1>
+  ) : (
+    <div className="logindiv">
+      <div>
+        <h6 className="form-heading">Welcome Back!</h6>
+        <form className="form" onSubmit={loginUser}>
+          <input
+            className="formm"
+            type="text"
+            name="username"
+            placeholder="Enter Email or Username"
+            required
+          />
+          <div className="password-wrapper">
+            <input
+              className="formm"
+              type={showPassword ? "text" : "password"}
+              name="password"
+              placeholder="Enter Password"
+              required
+            />
+            <FontAwesomeIcon
+              className={`togglePassword fa ${
+                showPassword ? "fa-eye-slash" : "fa-eye"
+              }`}
+              aria-label="Toggle password visibility"
+              onClick={handleTogglePasswordVisibility}
+              icon={showPassword ? faEyeSlash : faEye} // Use icons based on state
+            />
+          </div>
+          <button
+            className="forgotPassword"
+            type="button"
+          >
+            <Link to="/forgot-password">Forgot Password?</Link> {/* Use Link */}
+          </button>
+          <input
+            className="btn btn-success submitBtn"
+            type="submit"
+            value="Login"
+          />
+          <p className="new-account">
+            Don't have an account? <a href="/sign-up">Sign Up.</a>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
