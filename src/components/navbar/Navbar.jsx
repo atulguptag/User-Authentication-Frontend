5e4dd4eab687daf4aa9145243c7515c519423d56
Atul Gupta <atulguptag111@gmail.com>
2024-05-01 13:23:11 +0530

    using const instead of let and made some changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,10 +2,9 @@ import "./Navbar.css";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
-import { FaHome } from "react-icons/fa";
 
 const Navbar = () => {
-  let { user, logoutUser } = useContext(AuthContext);
+  const { user, logoutUser } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const toLogin = () => {
@@ -22,71 +21,66 @@ const Navbar = () => {
     navigate("/");
   };
   const toAdminPage = () => {
-    navigate("/admin-page");
+    navigate("/admin");
   };
   const toTicketPage = () => {
-    navigate("/Tickets");
+    navigate("/tickets");
   };
   return (
     <>
-      <nav className="navbar my-navbar">
-        <div className="container-fluid">
-          <div className="main-nav">
-            <div className="nav-1">{/* Your Logo goes here */}</div>
-            <div className="nav-2">
-              <div className="iconn">
-                <FaHome size={30} color="#373e98" onClick={toHome} />
+    <nav className="navbar my-navbar">
+      <div className="container-fluid">
+        <div className="main-nav">
+          <div className="nav-1">{/* Your Logo goes here */}</div>
+          <div className="nav-2">
+            <button className="btn" onClick={toHome}>
+              Home
+            </button>
+          </div>
+          <div className="nav-3">
+            {user ? (
+              <div className="login-btn">
+                {user.is_superuser ? (
+                  <button
+                    className="btn btn-outline-success admin m-2"
+                    onClick={toAdminPage}
+                  >
+                    Admin Panel
+                  </button>
+                ) : (
+                  <button
+                    className="btn btn-outline-success m-2"
+                    onClick={toTicketPage}
+                  >
+                    My Tickets
+                  </button>
+                )}
+                <button
+                  className="btn btn-outline-success logout-btn m-2"
+                  onClick={logout}
+                >
+                  Log Out
+                </button>
               </div>
-            </div>
-            <div className="nav-3">
-              {user ? (
-                <div className="login-btn">
-                  {user.is_superuser ? (
-                    <button
-                      className="btn btn-outline-success admin m-2"
-                      type="submit"
-                      onClick={toAdminPage}
-                    >
-                      Admin Panel
-                    </button>
-                  ) : (
-                    <button
-                      className="btn btn-outline-success m-2"
-                      type="submit"
-                      onClick={toTicketPage}
-                    >
-                      My Tickets
-                    </button>
-                  )}
-                  <button
-                    className="btn btn-outline-success m-2"
-                    type="submit"
-                    onClick={logout}
-                  >
-                    Log Out
-                  </button>
-                </div>
-              ) : (
-                <div className="login-btn">
-                  <button
-                    className="btn btn-outline-success m-2"
-                    type="submit"
-                    onClick={toLogin}
-                  >
-                    Log In
-                  </button>
-                  <button
-                    className="btn btn-outline-success m-2"
-                    type="submit"
-                    onClick={toSignup}
-                  >
-                    Sign Up
-                  </button>
-                </div>
-              )}
-            </div>
+            ) : (
+              <div className="login-btn">
+                <button
+                  className="btn btn-outline-success m-2"
+                  onClick={toLogin}
+                >
+                  Log In
+                </button>
+                <button
+                  className="btn btn-outline-success m-2"
+                  onClick={toSignup}
+                >
+                  Sign Up
+                </button>
+              </div>
+            )}
           </div>
         </div>
+      </div>
       </nav>
     </>
   );
