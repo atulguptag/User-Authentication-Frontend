f49940f6820450d43b482c0a3923e0493eafd072
Atul Gupta <atulguptag111@gmail.com>
2024-05-12 18:03:45 +0530

    Added footer element

--- src/components/mainpage/Mainpage.jsx
+++ src/components/mainpage/Mainpage.jsx
@@ -1,9 +1,11 @@
 import MoviesAndComingmovies from "./MoviesAndComingmovies";
+import Footer from "../footer/Footer";
 
 const Mainpage = () => {
   return (
     <div>
       <MoviesAndComingmovies />
+      <Footer/>
     </div>
   );
 };
