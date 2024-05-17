644f4334ae6ffdba44cb696bc0af3d6e1d8c2fcb
Atul Gupta <atulguptag111@gmail.com>
2024-05-17 16:41:25 +0530

    footer logo

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "./Footer.css";
+// import { useNavigate } from "react-router-dom";
+// import logo from "../../assets/images/moviepassa-logo.svg";
 
 const Footer = () => {
+  // const navigate = useNavigate();
+  // const toHome = () => {
+  //   navigate("/");
+  // };
+
   return (
     <footer className="footer">
       <div className="footer-container">
         <div className="footer-section">
           <h3 className="footer-section-title">MoviePassa</h3>
+          {/* <img src={logo} alt="Logo" className="nav-footer--icon" onClick={toHome}/> */}
           <ul className="footer-section-list">
             <li>
               <a href="/">About Us</a>
